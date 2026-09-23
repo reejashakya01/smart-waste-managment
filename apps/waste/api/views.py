fc0741b73156ab6784ac2ac96bd6736145c6f70c
reeja shakya <reejashakya099@gmail.com>
2026-09-23 13:12:54 +0545

    waste category post request

--- apps/waste/api/views.py
+++ apps/waste/api/views.py
@@ -8,10 +8,20 @@ from apps.waste.api.serializers import WasteCategorySerializer
 
 
 class WasteCategoryView(GenericAPIView):
-    queryset = WasteCategory
+    queryset = WasteCategory.objects.all()
     serializer_class = WasteCategorySerializer
 
     def get(self,request):
-        data = WasteCategory.objects.all()
-        serializer = WasteCategorySerializer(data, many=True)
+        data = self.get_queryset()
+        serializer = self.get_serializer(data, many=True)
         return Response(serializer.data,status.HTTP_200_OK)
+    
+    def post(self, request):   
+        data = request.data  
+        serializer = self.get_serializer(data=data) 
+        if serializer.is_valid():           
+            serializer.save()           
+            return Response({ "message":"Waste Category added successfully",
+                             "data":serializer.data }, 
+                            status.HTTP_201_CREATED)   
+            return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
