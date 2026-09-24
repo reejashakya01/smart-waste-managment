6c1681d02241329499153731d7e0a8807471ddec
reeja shakya <reejashakya099@gmail.com>
2026-09-24 12:42:44 +0545

    generic api view put, delete method

--- apps/waste/api/urls.py
+++ apps/waste/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-from apps.waste.api.views import WasteCategoryView
+from apps.waste.api.views import WasteCategoryView, WasteUpdateDetailDeleteView
 urlpatterns = [
-    path('', WasteCategoryView.as_view())
+    path('', WasteCategoryView.as_view()),
+     path('category', WasteCategoryView.as_view()),
+    path('category/<int:pk>', WasteUpdateDetailDeleteView.as_view())
 ]
--- apps/waste/api/views.py
+++ apps/waste/api/views.py
@@ -1,10 +1,11 @@
-
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from apps.waste.models import WasteCategory
 from apps.waste.api.serializers import WasteCategorySerializer
+from django.shortcuts import get_object_or_404
 
 
 class WasteCategoryView(GenericAPIView):
@@ -21,7 +22,46 @@ class WasteCategoryView(GenericAPIView):
         serializer = self.get_serializer(data=data) 
         if serializer.is_valid():           
             serializer.save()           
-            return Response({ "message":"Waste Category added successfully",
-                             "data":serializer.data }, 
-                            status.HTTP_201_CREATED)   
-            return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {
+                    "message": "Waste Category added successfully",
+                    "data": serializer.data,
+                },
+                status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+class WasteUpdateDetailDeleteView(GenericAPIView):
+    queryset = WasteCategory.objects.all()
+    serializer_class = WasteCategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
+        # data = get_object_or_404(WasteCategory, id=pk)
+                    # or
+        data = self.get_object()
+        serializer = self.get_serializer(data)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        waste_category = get_object_or_404(WasteCategory, id=pk)
+        serializer = self.get_serializer(waste_category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "message": "Waste Category updated successfully",
+                    "data": serializer.data,
+                },
+                status.HTTP_200_OK,
+            )
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self,request,pk):
+        waste_category = get_object_or_404(WasteCategory, id=pk)
+        waste_category.delete()
+        return Response({
+            "message":"Waste Category deleted successfully"
+        }, status.HTTP_200_OK)
